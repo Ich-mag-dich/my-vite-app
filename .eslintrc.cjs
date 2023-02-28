f319f40d0acc1806e9a7e98c237c492a4bf7eaf1
Ich-mag-dich <san02@kakao.com>
2023-02-28 18:58:53 +0900

    modified:   .eslintrc.cjs 	new file:   .prettierrc 	modified:   package.json 	modified:   src/App.tsx 	modified:   yarn.lock

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,27 @@
 module.exports = {
+  parser: '@typescript-eslint/parser',
   env: {
     browser: true,
     commonjs: true,
     es2021: true,
     node: true
   },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.ts', '.tsx']
+      }
+    }
+  },
   extends: [
+    'airbnb',
     'plugin:react/recommended',
-    'standard-with-typescript'
+    'standard-with-typescript',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint'
   ],
   overrides: [
   ],
@@ -15,8 +29,11 @@ module.exports = {
     ecmaVersion: 'latest'
   },
   plugins: [
-    'react'
+    'react',
+    '@typescript-eslint',
+    'prettier'
   ],
   rules: {
-  }
+  },
+  "ignorePatterns": ["build", "dist", "public"]
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import First from './components/test'
+import { useState } from "react";
+import "./App.css";
+import First from "./components/test";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <div className="card">
         <h3>{count}</h3>
-        <button onClick={() => {
-          if (count < 9) {
-            setCount((count) => count + 1  )
-          }
-        }}>
+        <button
+          onClick={() => {
+            if (count < 9) {
+              setCount((count) => count + 1);
+            }
+          }}
+        >
           +
         </button>
-        <button onClick={() => {
-          if (count > 0) {
-            setCount((count) => count - 1)
-          }
-        }}>
+        <button
+          onClick={() => {
+            if (count > 0) {
+              setCount((count) => count - 1);
+            }
+          }}
+        >
           -
         </button>
       </div>
-      <First  />
+      <First />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
